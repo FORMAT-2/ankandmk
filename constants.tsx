d059aeb9caceaa004a4f70b968b36e942681a6d3
Mayank Kumar <67382385+FORMAT-2@users.noreply.github.com>
2026-02-14 22:29:44 +0530

    Update text content and audio track URLs

--- constants.tsx
+++ constants.tsx
@@ -24,14 +24,14 @@ export const ICONS = {
 export const TEXT_CONTENT = {
   START: {
     title: "Hello, My Love!",
-    body: "I've prepared a little journey for us. Play these small games to unlock some of my favorite moments of us.",
+    body: "I've prepared a little journey for us. Thoda late ho gaya sorry meri jaan.",
     button: "Start the Journey"
   },
-  STAGE_1_MESSAGE: "You've caught my heart just like those falling ones! ❤️",
-  STAGE_2_MESSAGE: "Matching these was easy, but matching with you was fate. 💖",
-  STAGE_3_MESSAGE: "No matter how scrambled things get, my love for you is always clear. 💌",
+  STAGE_1_MESSAGE: "You've caught my heart just like those falling ones, I am glad that we made this far, aur mai ye hi chahtha hu bs isi tarah ham ssari life ssath rahe! ❤️",
+  STAGE_2_MESSAGE: "Matching these was easy, but matching with you was fate, Bby tumhe pata bhi nahi hai how much I love you. you are my everything my love, my sweetheart. 💖",
+  STAGE_3_MESSAGE: "No matter how scrambled things get, my love for you is always clear, Bby Aaj I waana say you something, mujhe meri life me kuch nahi chahiye ab bs tum chahiye aur baaki mai jo bhi kar rha hu bs tumhare liye kr rha hu mujhe jo chahiye tha wo mil gya hai bby ❤️😍💕. 💌",
   FINAL: {
-    title: "Happy Valentine's Day!",
+    title: "Happy Valentine's Day My swwetest love my bby , my world 🌍, my everthing, my sweetest reality 💕❤️!",
     body: "You've completed the journey, but our adventure together has only just begun. You are my greatest reward, my favorite memory, and the word that makes my life make sense.",
     replay: "Replay our story"
   }
@@ -60,9 +60,9 @@ export const GALLERY_IMAGES = {
  * AUDIO TRACKS
  */
 export const AUDIO_TRACKS = {
-  intro: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',      
-  gameplay: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',   
-  gallery: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3'     
+  intro: 'https://github.com/FORMAT-2/FORMAT-2/raw/refs/heads/main/HIGH%20ON%20YOU%20-%20Jind%20Universe%20%20Latest%20Punjabi%20Love%20Song%202024.mp3',      
+  gameplay: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',   
+  gallery: 'https://github.com/FORMAT-2/FORMAT-2/raw/refs/heads/main/Kya%20Tujhe%20Ab%20Ye%20Dil%20Bataye%20Sanam%20Re%20128%20Kbps.mp3'     
 };
 
 /**
